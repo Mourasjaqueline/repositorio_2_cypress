10271e754d2307529e78bda7baa1da49e52cbd57
Seu Nome <seu-email@exemplo.com>
2026-03-06 18:00:09 -0300

    alteração de arquivo config

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,8 @@ module.exports = defineConfig({
     chromeWebSecurity: false, // DESATIVA o bloqueio que causa o erro 401
     viewportWidth: 1280,      // Garante que a tela esteja larga para ver os botões
     viewportHeight: 720,
+    video: true, // Garante que o vídeo seja gravado
+    screenshotOnRunFailure: true, // Garante a foto se der erro
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
